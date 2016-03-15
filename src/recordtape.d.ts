beabbdcddc59001e23b8f1d5b51862eb2d029d8e
Willian Krueger <wkrueger128@gmail.com>
2016-03-15 14:31:08 -0300

    adicionar tipagem

---
+++ src/recordtape.d.ts
@@ -0,0 +1,53 @@
+declare interface RecordMeta {
+    fld : any;
+    sublists : any;
+    code : string;
+}
+
+declare interface RecordOpts {
+    noCache : boolean;
+}
+
+
+declare class Record {
+    
+    static meta : RecordMeta
+    static code : string;
+    static fld : any;
+    static sublists : string;
+    
+    meta : RecordMeta
+    code : string;
+    fld : any;
+    sublists : string;
+    
+    doCache : boolean;
+    
+    static create(code:string) : Record;
+    static fromId(id:number, opts? : RecordOpts) : Record;
+    static fromRecord(nlobjRecord:any) : Record;
+    static fromRecordSublist(spmSublist:any, id:number) : Record;
+    static fromSearchResult(nlobjSearchRes:any) : Record;
+    static fromCurrentClient(window?:Window) : Record;
+    
+    static curryf(field:string) : string;
+    
+    static end();
+    
+    f(field:string) : string;
+    fjoin(src:string,field:string) : string;
+    fset(field:string,value:any) : Record;
+    submit() : Record;
+    sublist(name : string, clas:any) : Record[];
+    
+}
+
+
+declare function factory(meta:RecordMeta) : typeof Record;
+
+export {
+    RecordMeta ,
+    RecordOpts ,
+    Record ,
+    factory
+}
